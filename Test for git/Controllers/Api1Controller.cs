dc1f77e298a227dfc3672a478775d9c00bea1412
Sherlock67 <alammushfiqul366@gmail.com>
2023-10-12 15:30:52 +0600

    testing for pull

---
+++ Test for git/Controllers/Api1Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test_for_git.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Api1Controller : ControllerBase
+    {
+        public Api1Controller()
+        {
+            
+        }
+    }
+}
